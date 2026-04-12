a04c9c1477a8aef915bf759f67d9b1f9a6920302
Janice91 <Kouakou.janice.2002@gmail.com>
2026-04-12 20:05:03 +0000

    Add python_async_function project tasks 0-4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that contains a task_wait_n function."""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn task_wait_random n times and return delays in ascending order."""
+    delays = await asyncio.gather(*[task_wait_random(max_delay) for _ in range(n)])
+    return sorted(delays)
